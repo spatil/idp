337a2c1b7f17b5ecc0c3fa05a0473ef1d8395b3f
Shailesh Patil <shailesh@quickinsure.co.in>
2023-10-31 17:44:20 +0530

    with gin

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/crewjam/saml/samlsp"
+	"github.com/gin-gonic/gin"
 )
 
 var (
@@ -20,8 +23,20 @@ var (
 )
 
 func main() {
+	router := gin.Default()
 
-	pfx := "./dp"
+	router.GET("/auth", PrepareSamlRequest())
+	router.POST("/auth/saml/acs", ParseSamlResponse())
+	router.Run(":8080")
+	//app := http.HandlerFunc(hello)
+	//http.Get("/auth", PrepareSamlRequest(samlSP))
+	//http.Handle("/auth", PrepareSamlRequest())
+	//http.Handle("/auth", samlSP.RequireAccount(app))
+	//http.Handle("/auth/saml/", ParseSamlResponse())
+
+}
+
+func SamlClient(pfx string) *samlsp.Middleware {
 	keyPair, err := tls.LoadX509KeyPair(fmt.Sprintf("%s.crt", pfx), fmt.Sprintf("%s.key", pfx))
 	if err != nil {
 		panic(err)
@@ -32,7 +47,7 @@ func main() {
 		panic(err) // TODO handle error
 	}
 
-	idpMetadataURL, err := url.Parse(oneLoginMetaUrl)
+	idpMetadataURL, err := url.Parse(cyberArkMetaUrl)
 	if err != nil {
 		panic(err)
 	}
@@ -57,22 +72,79 @@ func main() {
 		EntityID:    entityID,
 	})
 
-	app := http.HandlerFunc(hello)
-	http.Handle("/hello", samlSP.RequireAccount(app))
-	http.Handle("/saml/", samlSP)
-
-	http.ListenAndServe(":8080", nil)
+	acsUrl, _ := url.Parse("http://localhost:8080/auth/saml/acs")
+	samlSP.ServiceProvider.AcsURL = *acsUrl
+	return samlSP
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	s := samlsp.SessionFromContext(r.Context())
-	if s == nil {
-		return
-	}
-	sa, ok := s.(samlsp.SessionWithAttributes)
-	if !ok {
-		return
-	}
+func PrepareSamlRequest() gin.HandlerFunc {
+	log.Println("PrepareSamlRequest...")
+	samlSP := SamlClient("./dp")
+	return gin.HandlerFunc(func(c *gin.Context) {
+		c.SetCookie("saml-test", "1234", 3600, "/auth/saml/acs", "localhost", false, true)
+		samlSP.HandleStartAuthFlow(c.Writer, c.Request)
+	})
+}
 
-	fmt.Fprintf(w, "Token contents, %+v!", sa.GetAttributes())
+func ParseSamlResponse() gin.HandlerFunc {
+	samlSP := SamlClient("./dp")
+	fmt.Println("ParseSamlResponse...")
+	return gin.HandlerFunc(func(c *gin.Context) {
+		w := c.Writer
+		r := c.Request
+		/*
+			c, err := r.Cookie("saml-test")
+
+				if err != nil {
+					fmt.Println("Cookie error: ", err)
+					return // TODO handle error
+				}
+				if c.Value != "1234" {
+					fmt.Println("Cookie value error")
+
+				}*/
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Println("ParseForm error: ", err)
+			return
+		}
+		indexID := r.Form.Get("RelayState")
+
+		fmt.Printf("indexID: %+v\n", indexID)
+		trackedRequest, err := samlSP.RequestTracker.GetTrackedRequest(r, indexID)
+		if err != nil {
+			fmt.Println("GetTrackedRequest error: ", err)
+		}
+
+		fmt.Printf("trackedRequest: %+v\n", trackedRequest)
+		if err := samlSP.RequestTracker.StopTrackingRequest(w, r, indexID); err != nil {
+			fmt.Println("StopTrackingRequest error: ", err)
+		}
+
+		assertion, err := samlSP.ServiceProvider.ParseResponse(r, []string{trackedRequest.SAMLRequestID})
+
+		if err != nil {
+			fmt.Println("ParseResponse error: ", err)
+			return
+		}
+
+		attrs := map[string][]string{}
+
+		for i := range assertion.AttributeStatements[0].Attributes {
+			key := assertion.AttributeStatements[0].Attributes[i].Name
+			value := []string{}
+			for val := range assertion.AttributeStatements[0].Attributes[i].Values {
+				value = append(value, assertion.AttributeStatements[0].Attributes[i].Values[val].Value)
+			}
+			attrs[key] = value
+		}
+
+		fmt.Fprintf(w, "First Name: %s\n", attrs["first_name"][0])
+		fmt.Fprintf(w, "Last Name: %s\n", attrs["last_name"][0])
+		fmt.Fprintf(w, "Email: %s\n", attrs["email"][0])
+		fmt.Fprintf(w, "Groups: %s\n", strings.Join(attrs["groups"], ","))
+		fmt.Fprintf(w, "UserId: %s\n", attrs["user_id"][0])
+
+		//http.Redirect(w, r, c.Path, http.StatusFound)
+	})
 }
